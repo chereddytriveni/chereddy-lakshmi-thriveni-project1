afd4f4129cf29b1e8fa988e83ece1aae33cdd861
chereddytriveni <chereddytriveni30@gmail.com>
2025-09-24 08:34:08 +0530

    Add files via upload

---
+++ program-1.py
@@ -0,0 +1,23 @@
+#python programming language 
+class Calculator:
+    def __init__(self, a: float, b: float, operation: str):
+        self.a = a
+        self.b = b
+        self.operation = operation.lower()
+
+    def calculate(self):
+        if self.operation == "add":
+            return self.a + self.b
+        elif self.operation == "subtract":
+            return self.a - self.b
+        elif self.operation == "multiply":
+            return self.a * self.b
+        elif self.operation == "divide":
+            if self.b != 0:
+                return self.a / self.b
+            else:
+                return "Error: Division by zero"
+        else:
+            return "invalid operation"
+calc1 = Calculator(10, 5, "subtract")
+print(calc1.calculate())   
